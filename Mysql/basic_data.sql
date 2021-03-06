e7b626bf21de1e670081fe387e2ee8a0406a5927
achilleasein <achilleasein@gmail.com>
2021-03-06 19:15:31 +0200

    updating gitignore and mysql

---
+++ Mysql/basic_data.sql
@@ -0,0 +1,6 @@
+Use wallet_db;
+
+Create table `wallets` (id int, wallet varchar(20), money int);
+
+Insert into wallets (id, wallet, money) values (1, 'wallet-1', 0);
+Insert into wallets (id, wallet, money) values (2, 'wallet-2', 0);
