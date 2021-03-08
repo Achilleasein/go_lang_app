b838d23c402bbf07f2c16b6dd44cd4b49798a079
achilleasein <achilleasein@gmail.com>
2021-03-08 02:51:58 +0200

    Loggin and many many more

--- README.md
+++ README.md
@@ -8,3 +8,9 @@
 2. better have urls within quotation marks
 3. mysql default handler doesnt work due to issues with the path reading a @ randomly?!?!?!
 4. need to use go version 1.14 because 1.16 cant work inside a container, so ignore number 1
+5. Logrus gives the following error: 
+        root@e6fefefbdc1e:/go# go run main.go 
+        \# golang.org/x/sys/unix
+        src/golang.org/x/sys/unix/affinity_linux.go:14:20: undefined: _CPU_SETSIZE
+        src/golang.org/x/sys/unix/affinity_linux.go:14:35: undefined: _NCPUBITS
+        src/golang.org/x/sys/unix/affinity_linux.go:17:25: undefined: cpuMask 
--- go/main.go
+++ go/main.go
@@ -4,9 +4,11 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
-//	"gorm.io/gorm"
-	_  "gorm.io/driver/mysql"
+	"net/http"
+	"os"
 
+	_ "gorm.io/driver/mysql"
+	_ "gorm.io/gorm"
 	// _ "github.com/go-sql-driver/mysql"
 	// "gorm.io/gorm"
 	// "gorm.io/driver/sqlite"
@@ -14,7 +16,36 @@ import (
 	// "net/http"
 )
 
+var (
+	WarningLogger *log.Logger
+	InfoLogger    *log.Logger
+	ErrorLogger   *log.Logger
+)
+
+func init() {
+	file, err := os.OpenFile("logs.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	InfoLogger = log.New(file, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+	WarningLogger = log.New(file, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
+	ErrorLogger = log.New(file, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Println("Endpoint Hit: homePage")
+	InfoLogger.Println("Homepage requested")
+}
+
 func main() {
+	InfoLogger.Println("Starting the application...")
 	cnn, err := sql.Open("mysql", "docker:docker@tcp(db:3306)/wallet_db")
 	if err != nil {
 		log.Fatal(err)
@@ -28,4 +59,6 @@ func main() {
 	}
 
 	fmt.Println(id, wallet)
+
+	handleRequests()
 }
